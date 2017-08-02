aefc967e1a50d6e03f7e1fc4dbc3f01891b5940f
Meathill <meathill@gmail.com>
2017-08-02 23:38:02 +0800

    修正字体问题
    Signed-off-by: Meathill <meathill@gmail.com>

--- app/template.js
+++ app/template.js
@@ -1,9 +1,9 @@
 export let home = `
-<header class="animated slideInDown"><img src="{{./img/logo.png}}"></header>
+<header class="animated slideInDown"><img src="{{img/logo.png}}"></header>
 <div class="body animated zoomInDown">
-  <img src="{{./img/homepage.png}}">
+  <img src="{{img/homepage.png}}">
 </div>
-<footer class="animated slideInUp"><img src="{{./img/logos.png}}" ></footer>
+<footer class="animated slideInUp"><img src="{{img/logos.png}}" ></footer>
 `;
 
 export let nav = `
@@ -21,19 +21,19 @@ export let nav = `
 export let about = `
 <article>
 <p>磨铁成立于2007年12月12日。十年间，磨铁集团形成了磨铁图书、磨铁文学、磨铁娱乐三大基石业务板块。发展出一种文娱产业式开放生态，一端连接内容创作者，另一端连接更广泛的内容孵化者。通过磨铁，内容生态创作者可以在安心创作的同时得到商业价值的最大化，而磨铁也将与创作者携手一同打造更多属于中国的优秀原创内容。</p>
-<h2><img src="./img/logo-tushu.png" width="88"></h2>
+<h2><img src="img/logo-tushu.png" width="88"></h2>
 <p>磨铁起步于大众图书出版，经过多年的发展，已成为民营图书出版中的领军企业。2017年预计发货码洋将达到13亿人民币。</p>
-<h2><img src="./img/logo-wenxue.png" width="88"></h2>
+<h2><img src="img/logo-wenxue.png" width="88"></h2>
 <p>2011年磨铁进军网络文学，现已创建磨铁中文网、锦文小说网、墨墨言情网、逸云书院四大在线网站，并打造磨铁阅读和来看阅读两大阅读平台客户端沉淀深度用户。</p>
-<h2><img src="./img/logo-yule.png" width="90"></h2>
+<h2><img src="img/logo-yule.png" width="90"></h2>
 <p>2013年磨铁集团进军影视制作，利用IP资源储备进行影视剧项目开发。磨铁娱乐已先后成功出品电影《从你的全世界路过》《悟空传》；网剧《天意》《单恋大作战》，更有多部影视项目已进入全面开发阶段。磨铁娱乐以内容为基础，构建出完整的电影、电视剧、网剧及网大的开发链条，并以此为契机与更多合作伙伴连接，创造出更大IP的价值。</p>
 </article>
 `;
 
 export let ip100 = `
 <article>
-<h2><img src="{{./img/ip100.png}}"></h2>
-<ul style="background-image: url({{./img/frame.png}})">
+<h2><img src="{{img/ip100.png}}"></h2>
+<ul style="background-image: url({{img/frame.png}})">
 <li class="is-animated fadeInUp">十大主旋律正能量IP</li>
 <li class="is-animated fadeInUp">十大电影IP</li>
 <li class="is-animated fadeInUp">十大电视剧（网剧）IP</li>
@@ -51,9 +51,9 @@ export let ip100 = `
 
 export let invitation = `
 <div>
-  <img src="{{./img/title.png}}">
-  <img src="{{./img/invitation.png}}">
-  <img src="{{./img/logo-2.png}}">
+  <img src="{{img/title.png}}">
+  <img src="{{img/invitation.png}}">
+  <img src="{{img/logo-2.png}}">
 </div>
 `;
 
@@ -67,25 +67,25 @@ export let portfolio = `
 <div class="swiper-container">
   <div class="swiper-wrapper">
     <div class="swiper-slide">
-      <img src="{{./img/haibao1.jpg}}">
+      <img src="{{img/haibao1.jpg}}">
       <h3>电影《从你的全世界路过》</h3>
       <p>联合出品方：光线传媒<br>
 张一白执导；邓超领衔主演，白百何特约出演，杨洋、张天爱、岳云鹏、杜鹃、柳岩领衔主演</p>
     </div>
     <div class="swiper-slide">
-      <img src="{{./img/haibao2.jpg}}">
+      <img src="{{img/haibao2.jpg}}">
       <h3>电影《悟空传》</h3>
       <p>联合出品方：新丽传媒、上海三次元影业<br>
 郭子健执导；彭于晏、倪妮、欧豪、余文乐、郑爽、乔杉、杨迪联袂主演，俞飞鸿特别出演。</p>
     </div>
     <div class="swiper-slide">
-      <img src="{{./img/haibao3.jpg}}">
+      <img src="{{img/haibao3.jpg}}">
       <h3>超级网剧《天意》</h3>
       <p>联合出品方：优酷、青春你好<br>
 丁仰国执导，欧豪、海铃、李雨轩、孙珍妮、于济玮、田一彤、李感领衔主演；唐嫣、乔振宇、张丹峰、张睿特别出演。</p>
     </div>
     <div class="swiper-slide">
-      <img src="{{./img/haibao4.jpg}}">
+      <img src="{{img/haibao4.jpg}}">
       <h3>漫改剧《单恋大作战》</h3>
       <p>联合出品方：聚禾影画<br>
 沈沁源执导；由孔垂楠，刘美含，李鹤领衔主演；黄灿灿、张艺瀚特别出演。</p>
